9d19ec1635fa1bc8314a703d3b2a87206d93a076
NoSuchThingAsRandom <samralph76@gmail.com>
2017-09-19 19:33:18 +0100

    Task 4 complete.

--- Main/src/run.java
+++ Main/src/run.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 
 public class run {
 
+    public static void main(String[] args) {
+        run methods = new run();
+/*        methods.task1();
+        methods.task2();
+        methods.task3();*/
+        methods.task4();
+    }
+
     private void task1() {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the first number: ");
@@ -41,24 +49,30 @@ public class run {
         }
     }
 
-    private void task3(){
-        int[] diceRolls =new int[3];
-        Random rand=new Random();
-        for(int x=0;x<diceRolls.length;x++){
-            diceRolls[x]=rand.nextInt(6)+1;
-            System.out.println("Dice roll "+x+" was "+diceRolls[x]);
+    private void task3() {
+        int[] diceRolls = new int[3];
+        Random rand = new Random();
+        for (int x = 0; x < diceRolls.length; x++) {
+            diceRolls[x] = rand.nextInt(6) + 1;
+            System.out.println("Dice roll " + x + " was " + diceRolls[x]);
         }
-
-
     }
 
-
-
-    public static void main(String[] args) {
-        run methods =new run();
-/*        methods.task1();
-        methods.task2();*/
-        methods.task3();
-        //methods.task4();
+    private void task4(){
+        Scanner in =new Scanner(System.in);
+        System.out.println("Please enter the age of the dog: ");
+        try {
+        int dogAge=Integer.parseInt(in.nextLine());
+        int humanAge;
+        if(dogAge<=2){
+            humanAge=12*dogAge;
+        }else {
+            humanAge=24+(6*(dogAge-2));
+        }
+        System.out.println("The human age of "+dogAge+" is "+humanAge);
+        } catch (NumberFormatException exc) {
+            System.out.println("That is not a number,\nPlease try again.");
+            task1();
+        }
     }
 }
