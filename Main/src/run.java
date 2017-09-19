ff792386168dde401c6635d5bbe1513c73e3c9a3
NoSuchThingAsRandom <samralph76@gmail.com>
2017-09-19 19:24:44 +0100

    Re organised tasks

--- Main/src/run.java
+++ Main/src/run.java
@@ -1,7 +1,50 @@
+import java.util.Scanner;
+
 public class run {
 
+    private void task1() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        try {
+            int numA = Integer.parseInt(in.nextLine());
+            System.out.println("Enter the second number: ");
+            int numB = Integer.parseInt(in.nextLine());
+            if (numA > numB) {
+                System.out.println("The first number was biggest, " + numA);
+            } else if (numA == numB) {
+                System.out.println("The numbers are equal, " + numA + ", " + numB);
+            } else {
+                System.out.println("The second number was biggest, " + numB);
+            }
+        } catch (NumberFormatException exc) {
+            System.out.println("That is not a number,\nPlease try again.");
+            task1();
+        }
+    }
+
+    private void task2() {
+        String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the day: ");
+        try {
+            int num = Integer.parseInt(in.nextLine());
+            if (num < 7) {
+                System.out.println("The day is: " + days[num]);
+            } else {
+                System.out.println("That number is too big,\nPlease try again.");
+                task2();
+            }
+        } catch (NumberFormatException exc) {
+            System.out.println("That is not a number,\nPlease try again.");
+            task2();
+        }
+    }
+
+
+
     public static void main(String[] args) {
-        Task001.run();
-        Task002.run();
+        run methods =new run();
+        methods.task1();
+        methods.task2();
     }
 }
